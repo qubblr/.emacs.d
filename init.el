82e61bb5bd178d3bd90c3af7bf8db0fa0ee17664
Vladislav Kartashov <qubblr@me.com>
2016-12-15 00:50:39 +0300

    Keep custom settings in separate file

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; keep emacs custom settings in separate file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
