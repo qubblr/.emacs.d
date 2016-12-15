6811ce7cd91170525c6fa6dbd8bc044b1ad4832f
Vladislav Kartashov <qubblr@me.com>
2016-12-15 12:29:20 +0300

    Inhibit startup screen

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(setq inhibit-startup-screen t)
 
 ;; keep emacs custom settings in separate file
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
